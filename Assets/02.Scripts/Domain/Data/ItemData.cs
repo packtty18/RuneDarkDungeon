294a355573c067ab7e16422487309f7d4c4ae48f
roong-ji <mhienegjyuu@naver.com>
2026-01-05 18:32:44 +0900

    Feat: ItemData 비교 연산자 오버로딩

--- Assets/02.Scripts/Domain/Data/ItemData.cs
+++ Assets/02.Scripts/Domain/Data/ItemData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System;
 
 [System.Serializable]
-public class ItemData
+public class ItemData : IEquatable<ItemData>
 {
     [SerializeField] private int _id;
     [SerializeField] private EItemGrade _grade;
@@ -16,10 +16,27 @@ public class ItemData
         _grade = grade;
     }
 
-    public void GradeUp()
+    public void Upgrade()
     {
         if (_grade == EItemGrade.Legendary) return;
         ++_grade;
+    }
+
+    public bool Equals(ItemData other)
+    {
+        if (other == null) return false;
+        return _id == other._id && _grade == other._grade;
+    }
+    
+    public static bool operator ==(ItemData left, ItemData right)
+    {
+        if (ReferenceEquals(left, null)) return ReferenceEquals(right, null);
+        return left.Equals(right);
+    }
+    
+    public static bool operator !=(ItemData left, ItemData right)
+    {
+        return !(left == right);
     }
     
     public override string ToString() => $"[ID:{_id}] 등급:{_grade})";
