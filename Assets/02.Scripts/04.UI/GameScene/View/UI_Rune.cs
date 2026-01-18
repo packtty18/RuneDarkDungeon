4c79ee4c4a122c6789a467bbc12f897d808c4068
roong-ji <mhienegjyuu@naver.com>
2026-01-18 13:27:03 +0900

    Feat: 로비씬 장비 데이터 연동

---
+++ Assets/02.Scripts/04.UI/GameScene/View/UI_Rune.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Rune : MonoBehaviour
+{
+    [SerializeField] private GameObject _qRune;
+    [SerializeField] private GameObject _eRune;
+    [SerializeField] private GameObject _rRune;
+    [SerializeField] private Image _qRuneImage;
+    [SerializeField] private Image _eRuneImage;
+    [SerializeField] private Image _rRuneImage;
+
+    private IReadOnlyEquipment _equipment;
+
+    private void Start()
+    {
+        _equipment = DataManager.Instance.Equipment;
+        _equipment.Subscribe(Refresh);
+
+        Refresh();
+    }
+
+    private void OnDestroy()
+    {
+        _equipment.Unsubscribe(Refresh);
+    }
+
+    private void Refresh()
+    {
+        var qItem = _equipment.GetItem(ESkillSlot.Q);
+        if (qItem == null)
+        {
+            _qRune.SetActive(false);
+        }
+        else
+        {
+            _qRune.SetActive(true);
+            _qRuneImage.sprite = qItem.Icon;
+        }
+        
+        var eItem = _equipment.GetItem(ESkillSlot.E);
+        if (eItem == null)
+        {
+            _qRune.SetActive(false);
+        }
+        else
+        {
+            _qRune.SetActive(true);
+            _qRuneImage.sprite = eItem.Icon;
+        }
+        
+        var rItem = _equipment.GetItem(ESkillSlot.R);
+        if (rItem == null)
+        {
+            _qRune.SetActive(false);
+        }
+        else
+        {
+            _qRune.SetActive(true);
+            _qRuneImage.sprite = rItem.Icon;
+        }
+    }
+}
