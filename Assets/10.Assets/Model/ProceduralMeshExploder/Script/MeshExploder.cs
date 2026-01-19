3a6a649fb09a9362659d761cebf8074cb02ebaea
PSH <packtty18@naver.com>
2026-01-19 19:37:36 +0900

    Feat: 프롭에서 뜨는 오류 해결

--- Assets/10.Assets/Model/ProceduralMeshExploder/Script/MeshExploder.cs
+++ Assets/10.Assets/Model/ProceduralMeshExploder/Script/MeshExploder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+using Sirenix.OdinInspector;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -14,6 +15,7 @@ namespace ProceduralMeshExploder
         Renderer parentRenderer;
         bool animating;
 
+        [Button]
         public void Explode()
         {
             gameObject.SetActive(true);
