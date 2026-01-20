733fd07c38265cfbf031aa91bbd09717775f5623
roong-ji <mhienegjyuu@naver.com>
2026-01-20 15:32:47 +0900

    Feat: 스크롤 자동 이동 추가

--- Assets/02.Scripts/04.UI/GameScene/Presenter/RewardPresenter.cs
+++ Assets/02.Scripts/04.UI/GameScene/Presenter/RewardPresenter.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class RewardPresenter : MonoBehaviour
 {
@@ -7,10 +8,15 @@ public class RewardPresenter : MonoBehaviour
     [SerializeField] private UI_SequencePopup _sequencePopup;
     [SerializeField] private UI_SlotContainer _rewardUI;
     [SerializeField] private UI_GoldText _rewardGoldTextUI;
-
+    
+    [Space]
+    [SerializeField] private float _startDelay = 1f;
+    
     private IReadOnlyList<ItemData> _items;
     private IReadOnlyCurrency _gold;
 
+    private Tween _delayedShow;
+    
     public void Initialize(IReadOnlyList<ItemData> items, IReadOnlyCurrency gold)
     {
         _items = items;
@@ -20,8 +26,13 @@ public class RewardPresenter : MonoBehaviour
     public void ShowReward()
     {
         _rewardUI.Refresh(_items);
-        _rewardGoldTextUI.Refresh(_gold.Amount);
         
-        _sequencePopup.PlayAnimation();
+        _delayedShow?.Kill();
+        _delayedShow = DOVirtual.DelayedCall(_startDelay, () =>
+        {
+            _rewardUI.Show();
+            _sequencePopup.PlayAnimation();
+            _rewardGoldTextUI.Refresh(_gold.Amount);
+        });
     }
 }
--- Assets/02.Scripts/04.UI/GameScene/View/UI_SequencePopup.cs
+++ Assets/02.Scripts/04.UI/GameScene/View/UI_SequencePopup.cs
@@ -2,49 +2,51 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
 
-public class UI_SequencePopup : PUBase
+public class UI_SequencePopup : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Transform _slotParent;
     [SerializeField] private ScrollRect _scrollRect;
     
     [Header("Effect Settings")]
-    [SerializeField] private float _startDelay = 1f;
     [SerializeField] private float _interval = 0.1f;
     [SerializeField] private float _scaleTime = 0.3f;
     [SerializeField] private Ease _easeType = Ease.OutBack;
 
-    private Tween _delayedStart;
-    private Sequence _popSequence;
+    private Sequence _sequence;
     
     public void PlayAnimation()
     {
-        _delayedStart?.Kill();
-        _delayedStart = DOVirtual.DelayedCall(_startDelay, () =>
-        {
-            Show();
-            PlayPopSequence();
-        });
-    }
-
-    private void PlayPopSequence()
-    {
-        _popSequence?.Kill();
-        _popSequence = DOTween.Sequence();
+        _sequence?.Kill();
+        _sequence = DOTween.Sequence();
         
         _scrollRect.verticalNormalizedPosition = 1f;
         
-        int count = 5;
+        int count = 0;
         for (int i = 0; i < _slotParent.childCount; i++)
         {
             Transform child = _slotParent.GetChild(i);
             child.localScale = Vector3.zero;
             float startTime = count * _interval;
-            _popSequence.Insert(startTime, child.DOScale(1f, _scaleTime).SetEase(_easeType));
+            _sequence.Insert(startTime, child.DOScale(1f, _scaleTime).SetEase(_easeType));
             count++;
         }
         
-        _popSequence.OnComplete(() => 
+        Canvas.ForceUpdateCanvases(); 
+        bool needScroll = _scrollRect.content.rect.height > _scrollRect.viewport.rect.height;
+
+        if (needScroll)
+        {
+            float totalAnimDuration = _slotParent.childCount * _interval;
+            
+            float scrollDelay = _interval; 
+
+            _sequence.Insert(scrollDelay, 
+                _scrollRect.DOVerticalNormalizedPos(0f, totalAnimDuration - scrollDelay)
+                    .SetEase(Ease.InOutQuad));
+        }
+        
+        _sequence.OnComplete(() => 
         {
             CursorManager.Instance?.SetCursorLock(false);
         });
